e4ec5b1c3722f334186d22a04faf7c5e26b12641
suhyeon <128122087+SuhyeonB@users.noreply.github.com>
2025-04-17 11:11:26 +0900

    [level 2] Title: 피로도, Time: 3.97 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,24 +1,21 @@
 class Solution {
-    int answer = 0;
-
     public int solution(int k, int[][] dungeons) {
         boolean[] visited = new boolean[dungeons.length];
-        dfs(k, 0, dungeons, visited);
-        return answer;
+        return dfs(k, 0, visited, dungeons);
     }
-
-    public void dfs(int k, int count, int[][] dungeons, boolean[] visited) {
-        answer = Math.max(answer, count);
-
-        for (int i = 0; i < dungeons.length; i++) {
-            int min = dungeons[i][0];
-            int cost = dungeons[i][1];
-
-            if (!visited[i] && k >= min) {
-                visited[i] = true;
-                dfs(k - cost, count + 1, dungeons, visited);
-                visited[i] = false;
+    
+    public int dfs(int k, int cnt, boolean[] visited, int[][] dungeons) {
+        int answer = cnt;
+        for (int i=0; i<dungeons.length; i++) {
+            if(k>=dungeons[i][0] && !visited[i]) { // 피로도가 충분하고 방문한 적 없다면
+                visited[i] = true;  // 방문
+                int a = dfs(k-dungeons[i][1], cnt+1, visited, dungeons);    // 다음 던전 방문
+                answer = Math.max(answer, a);
+                // 방문에 실패할 경우
+                visited[i] = false; // backtracking
+                
             }
         }
+        return answer;
     }
 }
