be3ef8d21445b4f0e0ada26a66599d09bda8f36c
suhyeon <128122087+SuhyeonB@users.noreply.github.com>
2025-06-02 12:18:34 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 28.44 ms, Memory: 91.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        List<Integer> answer = new ArrayList<>();
+        for(int i=5; i<num_list.length; i++) answer.add(num_list[i]);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
